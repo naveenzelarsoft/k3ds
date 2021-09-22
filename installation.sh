65559fe55e49ec2bcecd6035fc0de688cb80dccb
naveen <naveen@gmail.com>
2021-09-22 17:17:51 +0530

    k8s

--- installation.sh
+++ installation.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 apt update -y
-sudo apt-get install -y && apt-transport-https -y
-    && ca-certificates -y
-    && curl -y
-    && gnupg -y
-    && lsb-release -y
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
